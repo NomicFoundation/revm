bdc2cc6ee311e2a18b5186b0820195c177ed5788
Wodann <Wodann@users.noreply.github.com>
2025-01-14 18:16:27 +0000

    feat: expose local state from Journal

--- crates/context/src/journaled_state.rs
+++ crates/context/src/journaled_state.rs
@@ -73,6 +73,10 @@ impl<DB: Database> Journal for JournaledState<DB> {
         &mut self.database
     }
 
+    fn state(&self) -> &EvmState {
+        &self.state
+    }
+
     fn sload(
         &mut self,
         address: Address,
